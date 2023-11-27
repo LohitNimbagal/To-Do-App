ad1284593477eb6e13db051709273b36f728dcd1
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2023-11-27 22:45:42 +0530

    todo list

---
+++ script.js
@@ -0,0 +1,57 @@
+let addList = document.querySelector('.addList')
+const list = document.querySelector('.list')
+let newList
+
+const unCheckImg = '/images/uncheck.png'
+
+const addToList = () => {
+
+    if (addList.value !== "") {
+        newList = document.createElement('li')
+        let span = document.createElement('span')
+
+        newList.innerHTML = addList.value
+        list.appendChild(newList)
+
+        span.innerHTML = '\u00d7'
+        newList.appendChild(span);
+
+        addList.value = "";
+        saveData();
+    } else {
+        alert('Enter Valid Input')
+    }
+
+}
+
+
+list.addEventListener('click', (e) => {
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle('checked');
+        saveData()
+    } else if (e.target.tagName == 'SPAN') {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false)
+
+document.querySelector('.addBtn').addEventListener('click', () => {
+    addToList();
+    saveData()
+})
+addList.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addToList();
+        saveData()
+    }
+})
+
+function saveData() {
+    localStorage.setItem('data', list.innerHTML);
+}
+
+function showData() {
+    list.innerHTML = localStorage.getItem('data');
+}
+
+showData()
